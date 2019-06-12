39816edec01bbceb92d657f0413dd4dbed5ca802
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-06-12 16:20:11 -0400

    Updates

---
+++ precache-manifest.cb9b3b2aa514b5e66627a6300e49ccc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f966c55fd44c31996597aaff9f8c512d",
+    "url": "/lombardpress-web.js/index.html"
+  },
+  {
+    "revision": "64479b3cb0a530258c53",
+    "url": "/lombardpress-web.js/static/css/2.54ea85e9.chunk.css"
+  },
+  {
+    "revision": "a050f00ae5a0c2260205",
+    "url": "/lombardpress-web.js/static/css/main.94c60737.chunk.css"
+  },
+  {
+    "revision": "64479b3cb0a530258c53",
+    "url": "/lombardpress-web.js/static/js/2.738eec86.chunk.js"
+  },
+  {
+    "revision": "a050f00ae5a0c2260205",
+    "url": "/lombardpress-web.js/static/js/main.f6e16ca6.chunk.js"
+  },
+  {
+    "revision": "d2d52d88a6e3b887fb58",
+    "url": "/lombardpress-web.js/static/js/runtime~main.e4ab7b1b.js"
+  }
+]);
