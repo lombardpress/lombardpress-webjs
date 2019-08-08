1facae2937ef0b67605cfcbdf664aff8f3a37f72
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-08-08 08:22:36 -0400

    removed width adjustment in image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,9 +5,11 @@ function Image(props) {
   let newCoords = ""
   if (props.coords){
     const coordsSplitArray = props.coords.split(",")
-    const x = parseInt(coordsSplitArray[0]) - 20
+    //const x = parseInt(coordsSplitArray[0]) - 20
+    const x = parseInt(coordsSplitArray[0])
     const y = coordsSplitArray[1]
-    const w = parseInt(coordsSplitArray[2]) + 40
+    //const w = parseInt(coordsSplitArray[2]) + 40
+    const w = parseInt(coordsSplitArray[2])
     const h = coordsSplitArray[3]
     newCoords = x + "," + y + "," + w + "," + h
   }
