fafc324baf26869af39b5241f232318b3f22ee67
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-11-02 13:42:58 -0400

    updates to levenshein distance and bitmap and package.json

--- src/components/TextCompareItem.js
+++ src/components/TextCompareItem.js
@@ -16,7 +16,8 @@ class TextCompareItem extends React.Component {
       showCompare: true,
       compareText: "",
       rawText: "",
-      show: true
+      show: true,
+      levenshteinDistance: undefined
     }
   }
   handleToggleShow(){
@@ -49,9 +50,10 @@ class TextCompareItem extends React.Component {
             const diff = dmp.diff_main(this.textClean(base), this.textClean(text.data));
             // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
             dmp.diff_cleanupSemantic(diff);
+            const levenshteinDistance = dmp.diff_levenshtein(diff)
             const ds = dmp.diff_prettyHtml(diff);
             if (this.mounted === true && base){
-              this.setState({compareText: ds, rawText: text.data})
+              this.setState({compareText: ds, rawText: text.data, levenshteinDistance: levenshteinDistance})
             }
             else if(this.mounted){
               this.setState({rawText: text.data})
@@ -71,6 +73,7 @@ class TextCompareItem extends React.Component {
       this.setState({rawText: "", compareText: ""})
       this.createCompare(newProps.base, newProps.compareTranscription)
     }
+
   }
   componentWillUnmount(){
       this.mounted = false;
@@ -81,14 +84,19 @@ class TextCompareItem extends React.Component {
     const displayComparison = () => {
       const isBase = this.props.base === this.state.rawText
       if (this.state.showCompare && this.props.base && this.state.compareText){
+        const levenNum = this.state.levenshteinDistance
+        const heatColor = levenNum * 5
         return (
-          <div>
+          <div style={{borderLeft: "5px solid rgba(" + heatColor + ", 0, 255, 1)", paddingLeft: "5px"}}>
             <span><Link to={"/text?resourceid=" + this.props.compareTranscription}>{this.props.compareTranscription}</Link> </span>
-            | <span className="lbp-span-link" title="show/hide" onClick={() => this.handleToggleShow()}>{this.state.show ? <FaEyeSlash/> : <FaEye/>}</span>
+            <span className="lbp-span-link" title="show/hide" onClick={() => this.handleToggleShow()}>{this.state.show ? <FaEyeSlash/> : <FaEye/>}</span>
             <span className="lbp-span-link" title="toggle comparison off" onClick={() => this.handleToggleCompare()}><FaToggleOn/></span>
             {!isBase && <span className="lbp-span-link" title="set as base" onClick={() => this.props.handleChangeBase(this.state.rawText)}><FaStar/></span>}
+            <span style={{fontSize: "14px", paddingLeft: "4px"}} title="levenshtein distance">{levenNum}</span>
             <div className={this.state.show ? "unhidden" : "hidden"}>
               <div ref="text" dangerouslySetInnerHTML={{ __html: this.state.compareText}}></div>
+
+
             </div>
           </div>
         )
