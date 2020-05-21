5274d836c8daaf6a0196e4e33b2afdf4c611634d
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2020-05-21 08:08:01 -0400

    converted TextNavBar to functional component; added TextOutline test

--- src/components/TextNavBar.js
+++ src/components/TextNavBar.js
@@ -5,35 +5,23 @@ import Nav from 'react-bootstrap/Nav';
 import { withTranslation } from 'react-i18next';
 
 
-class TextNavBar extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      next: "",
-      previous: "",
-      mtFocus: ""
-    }
-  }
-  UNSAFE_componentWillReceiveProps(nextProps){
-    this.setState({next: nextProps.next, previous: nextProps.previous, mtFocus: nextProps.mtFocus})
-  }
-  render(){
-    const { t } = this.props;
+const TextNavBar = (props) => {
+  
+    const { t } = props;
     return (
       <Navbar bg="light" variant="light" expand="lg" fixed="bottom">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          {this.props.previous && <NavLink className="nav-link" to={"/text?resourceid=" + this.props.previous + this.props.mtFocus}>{t("Previous")}</NavLink>}
-          {this.props.next && <NavLink className="nav-link" to={"/text?resourceid=" + this.props.next + this.props.mtFocus}>{t("Next")}</NavLink>}
-          {this.props.topLevel && <NavLink className="nav-link" to={"/text?resourceid=" + this.props.topLevel}>{t("Text Home")}</NavLink>}
-          {this.props.pdfView ? <span className="nav-link" onClick={()=>this.props.handleTogglePdfView()}>{t("Web View")}</span> : <span className="nav-link" onClick={()=>this.props.handleTogglePdfView()}>{t("Print View")}</span>}
+          {props.previous && <NavLink className="nav-link" to={"/text?resourceid=" + props.previous + props.mtFocus}>{t("Previous")}</NavLink>}
+          {props.next && <NavLink className="nav-link" to={"/text?resourceid=" + props.next + props.mtFocus}>{t("Next")}</NavLink>}
+          {props.topLevel && <NavLink className="nav-link" to={"/text?resourceid=" + props.topLevel}>{t("Text Home")}</NavLink>}
+          {props.pdfView ? <span className="nav-link" onClick={()=>props.handleTogglePdfView()}>{t("Web View")}</span> : <span className="nav-link" onClick={()=>props.handleTogglePdfView()}>{t("Print View")}</span>}
         </Nav>
         </Navbar.Collapse>
       </Navbar>
 
     );
-  }
 }
 
 export default withTranslation()(TextNavBar);
