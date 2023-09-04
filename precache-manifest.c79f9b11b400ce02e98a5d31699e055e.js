c2ed6bbd94837f27bd6552d45f8b8de405a2c5bd
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2023-09-04 07:53:51 -0400

    Updates

---
+++ precache-manifest.c79f9b11b400ce02e98a5d31699e055e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74638521bff205df1fbde308cbedf82a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0d13fb868815911d13",
+    "url": "/static/css/2.72f7d400.chunk.css"
+  },
+  {
+    "revision": "cbcbf16c22e1558b01a8",
+    "url": "/static/css/main.44f28e8f.chunk.css"
+  },
+  {
+    "revision": "dc0d13fb868815911d13",
+    "url": "/static/js/2.eef20d90.chunk.js"
+  },
+  {
+    "revision": "f6a1cc41a1c9753fac520997054d2202",
+    "url": "/static/js/2.eef20d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcbf16c22e1558b01a8",
+    "url": "/static/js/main.5ecea55a.chunk.js"
+  },
+  {
+    "revision": "819e00913a1d8adc4148",
+    "url": "/static/js/runtime-main.e3e30fd3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56fc16f0352fad6cc0aff54f6541cc53.js"
+  "/precache-manifest.c79f9b11b400ce02e98a5d31699e055e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
